ad7324c1d564ff9f2f3ea8cd73240e397fd954ce
panduramadhan2 <panduramadhan2@gmail.com>
2023-11-10 13:37:40 +0700

    testing deploy stripe secreet webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,69 +1,140 @@
-import Stripe from "stripe"
-import { headers } from "next/headers"
-import { NextResponse } from "next/server"
+// import Stripe from "stripe"
+// import { headers } from "next/headers"
+// import { NextResponse } from "next/server"
 
-import { stripe } from "@/lib/stripe"
-import prismadb from "@/lib/prismadb"
+// import { stripe } from "@/lib/stripe"
+// import prismadb from "@/lib/prismadb"
+
+// export async function POST(req: Request) {
+//   const body = await req.text()
+//   const signature = headers().get("Stripe-Signature") as string
+
+//   let event: Stripe.Event
+
+//   try {
+//     event = stripe.webhooks.constructEvent(
+//       body,
+//       signature,
+//       process.env.STRIPE_WEBHOOK_SECRET!
+//     )
+//   } catch (error: any) {
+//     return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
+//   }
+
+//   const session = event.data.object as Stripe.Checkout.Session;
+//   const address = session?.customer_details?.address;
+
+//   const addressComponents = [
+//     address?.line1,
+//     address?.line2,
+//     address?.city,
+//     address?.state,
+//     address?.postal_code,
+//     address?.country
+//   ];
+
+//   const addressString = addressComponents.filter((c) => c !== null).join(', ');
+
+
+//   if (event.type === "checkout.session.completed") {
+//     const order = await prismadb.order.update({
+//       where: {
+//         id: session?.metadata?.orderId,
+//       },
+//       data: {
+//         isPaid: true,
+//         addres: addressString,
+//         phone: session?.customer_details?.phone || '',
+//       },
+//       include: {
+//         orderItems: true,
+//       }
+//     });
+
+//     const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+//     await prismadb.product.updateMany({
+//       where: {
+//         id: {
+//           in: [...productIds],
+//         },
+//       },
+//       data: {
+//         isArchived: true
+//       }
+//     });
+//   }
+
+//   return new NextResponse(null, { status: 200 });
+// };
+
+import { NextResponse } from "next/server";
+import { stripe } from "@/lib/stripe";
+import Stripe from 'stripe';
+import prismadb from "@/lib/prismadb";
 
 export async function POST(req: Request) {
-  const body = await req.text()
-  const signature = headers().get("Stripe-Signature") as string
+  const body = await req.text();
+  const signature = (req.headers.get("Stripe-Signature") as string) || "";
 
-  let event: Stripe.Event
+  console.log('Received webhook payload:', body);
+  console.log('Received webhook signature:', signature);
 
   try {
-    event = stripe.webhooks.constructEvent(
+    const event = stripe.webhooks.constructEvent(
       body,
       signature,
-      process.env.STRIPE_WEBHOOK_SECRET!
-    )
-  } catch (error: any) {
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
-  }
+      process.env.STRIPE_WEBHOOK_SECRET!.trim()
+    );
 
-  const session = event.data.object as Stripe.Checkout.Session;
-  const address = session?.customer_details?.address;
+    console.log('Webhook event:', event);
 
-  const addressComponents = [
-    address?.line1,
-    address?.line2,
-    address?.city,
-    address?.state,
-    address?.postal_code,
-    address?.country
-  ];
+    const session = event.data.object as Stripe.Checkout.Session;
+    const address = session?.customer_details?.address;
 
-  const addressString = addressComponents.filter((c) => c !== null).join(', ');
+    const addressComponents = [
+      address?.line1,
+      address?.line2,
+      address?.city,
+      address?.state,
+      address?.postal_code,
+      address?.country
+    ];
 
+    const addressString = addressComponents.filter((c) => c !== null).join(', ');
 
-  if (event.type === "checkout.session.completed") {
-    const order = await prismadb.order.update({
-      where: {
-        id: session?.metadata?.orderId,
-      },
-      data: {
-        isPaid: true,
-        addres: addressString,
-        phone: session?.customer_details?.phone || '',
-      },
-      include: {
-        orderItems: true,
-      }
-    });
-
-    const productIds = order.orderItems.map((orderItem) => orderItem.productId);
-
-    await prismadb.product.updateMany({
-      where: {
-        id: {
-          in: [...productIds],
+    if (event.type === "checkout.session.completed") {
+      const order = await prismadb.order.update({
+        where: {
+          id: session?.metadata?.orderId,
         },
-      },
-      data: {
-        isArchived: true
-      }
-    });
-  }
+        data: {
+          isPaid: true,
+          addres: addressString,
+          phone: session?.customer_details?.phone || '',
+        },
+        include: {
+          orderItems: true,
+        }
+      });
 
-  return new NextResponse(null, { status: 200 });
-};
+      const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+      await prismadb.product.updateMany({
+        where: {
+          id: {
+            in: [...productIds],
+          },
+        },
+        data: {
+          isArchived: true
+        }
+      });
+    }
+
+    return new NextResponse(null, { status: 200 });
+  } catch (error: any) {
+    console.error('Webhook processing error:', error);
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+  }
+}
